4dc28aa4e5d8062d0addabc32030d47634f61c3f
Nikola Talevski <talevskinikola1409@gmail.com>
2025-01-29 16:38:34 +0100

    Update evaluation approach

--- arena/evaluation.py
+++ arena/evaluation.py
@@ -2,7 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('championship_10000_3.csv')
+df1 = pd.read_csv('championship_10000.csv')
+df2 = pd.read_csv('championship_10000_2.csv')
+df3 = pd.read_csv('championship_10000_3.csv')
+
+df = pd.concat([df1, df2, df3], ignore_index=True)
 
 players = pd.concat([
     df[['agent1', 'agent1 sum of rewards']].rename(columns={'agent1': 'player', 'agent1 sum of rewards': 'rews'}),
@@ -10,36 +14,31 @@ players = pd.concat([
 ])
 
 players['rews_per_game'] = players['rews'] / 10000
+players["agent_id"] = players["player"].str.split(":").str[0]
 
-player_stats = players.groupby('player').agg(
+player_stats = players.groupby('agent_id').agg(
     total_rewards=('rews', 'sum'),
     average_rewards=('rews_per_game', 'mean')
 ).reset_index()
 
-best_player = player_stats.loc[player_stats['total_rewards'].idxmax(), 'player']
+# best_player = player_stats.loc[player_stats['total_rewards'].idxmax(), 'agent_id']
+#
+# sns.barplot(data=player_stats, x='agent_id', y='total_rewards', palette='viridis')
+# plt.title('Total Rewards per Player')
+# plt.ylabel('Total Rewards')
+# plt.xlabel('Player')
+# plt.show()
 
-plt.figure(figsize=(15, 10))
-
-plt.subplot(3, 1, 1)
-sns.barplot(data=player_stats, x='player', y='total_rewards', palette='viridis')
-plt.title('Total Rewards per Player')
-plt.ylabel('Total Rewards')
-plt.xlabel('Player')
-
-plt.subplot(3, 1, 2)
 sns.lineplot(
     data=player_stats,
-    x='player',
+    x='agent_id',
     y='average_rewards',
     marker='o',
     linewidth=2.5,
     color='green'
 )
-plt.title('Average Rewards per Game per Player')
-plt.ylabel('Average Rewards')
-plt.xlabel('Player')
-
-plt.suptitle(f'The Best Player is: {best_player}', fontsize=16, fontweight='bold', color='darkgreen')
-plt.tight_layout(rect=(0, 0, 1, 0.95))
-
+plt.ylabel('Просечна награда')
+plt.xlabel('Агент')
 plt.show()
+
+print(player_stats)
